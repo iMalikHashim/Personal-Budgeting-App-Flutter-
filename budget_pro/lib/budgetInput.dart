c0e938c8fa477e4ceac04f732b429da30b7cb445
iMalikHashim <hashimtop10@gmail.com>
2024-08-04 11:02:36 +0500

    Added Date and Logic

--- budget_pro/lib/budgetInput.dart
+++ budget_pro/lib/budgetInput.dart
@@ -1,4 +1,6 @@
+import 'package:budget_pro/model/model.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Input extends StatefulWidget {
   const Input({super.key});
@@ -10,6 +12,18 @@ class Input extends StatefulWidget {
 class _InputState extends State<Input> {
   final _titleController = TextEditingController();
   final _spentAmountController = TextEditingController();
+  DateTime? _pickedDate;
+
+  void _datePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    DateTime? selectedDate = await showDatePicker(
+        context: context, firstDate: firstDate, lastDate: now);
+
+    setState(() {
+      _pickedDate = selectedDate;
+    });
+  }
 
   @override
   void dispose() {
@@ -21,34 +35,64 @@ class _InputState extends State<Input> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Expanded(
-        child: Column(
-          children: [
-            TextField(
-              maxLength: 50,
-              controller: _titleController,
-              decoration: const InputDecoration(
-                hintText: "Bought Something...",
-              ),
+      padding: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 10.0),
+      child: Column(
+        children: [
+          TextField(
+            maxLength: 50,
+            controller: _titleController,
+            decoration: const InputDecoration(
+              hintText: "Title: Bought Something...",
             ),
-            Row(
-              children: [
-                TextField(
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
                   controller: _spentAmountController,
                   decoration: const InputDecoration(
                     hintText: "250",
-                    prefixText: "Rs.",
+                    prefixText: "Rs. ",
                   ),
-                )
-              ],
-            ),
-            ElevatedButton(
-              onPressed: () => print(_titleController.text),
-              child: Text("Print"),
-            ),
-          ],
-        ),
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      _pickedDate == null
+                          ? "No Date Selected!"
+                          : fDate.format(_pickedDate!),
+                    ),
+                    IconButton(
+                      onPressed: _datePicker,
+                      icon: Icon(Icons.calendar_month),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () => print(_titleController.text),
+                child: const Text("Print"),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              ElevatedButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text("Cancel"),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
