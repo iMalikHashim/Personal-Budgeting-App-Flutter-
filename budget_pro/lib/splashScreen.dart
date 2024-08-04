d12a6ae1b6e362d563e6b5487f77ccdcd3cde4ba
iMalikHashim <hashimtop10@gmail.com>
2024-08-04 23:50:26 +0500

    UI Update(2)

--- budget_pro/lib/splashScreen.dart
+++ budget_pro/lib/splashScreen.dart
@@ -17,13 +17,26 @@ class SplashScreen extends StatelessWidget {
       // backgroundColor: Color(0xFF055B5C),
       backgroundColor: Color.fromARGB(255, 255, 255, 255),
       body: Center(
-        child: Text(
-          'Budget Pro',
-          style: TextStyle(
-            fontSize: 46.0,
-            fontWeight: FontWeight.bold,
-            color: Color(0xFF055B5C),
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Budget Pro',
+              style: TextStyle(
+                fontSize: 46.0,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF055B5C),
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Icon(
+              Icons.calculate_rounded,
+              size: 200,
+              color: Colors.teal,
+            ),
+          ],
         ),
       ),
     );
